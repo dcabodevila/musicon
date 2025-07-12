8b2060960dc4b01b14aaee019688d51aed31325f
dcabodevila <dcabodevila@gmail.com>
2025-07-12 21:27:41 +0200

    optimizacion controlleradvice

--- src/main/java/es/musicalia/gestmusica/config/GlobalControllerAdvice.java
+++ src/main/java/es/musicalia/gestmusica/config/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package es.musicalia.gestmusica.config;
 
 import es.musicalia.gestmusica.auth.model.CustomAuthenticatedUser;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,56 +10,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static es.musicalia.gestmusica.util.GestmusicaUtils.isUserAutheticated;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+    @ModelAttribute
+    public void addGlobalAttributes(@AuthenticationPrincipal CustomAuthenticatedUser user , Model model) {
 
-    @ModelAttribute("imagenUsuarioAutenticado")
-    public String addImagenUsuarioAutenticado() {
-        if (isUserAutheticated()) {
-
-            return ((CustomAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
-                    .getUsuario().getImagen();
-
-        }
-        return null;
+            if (user != null) {
+                model.addAttribute("imagenUsuarioAutenticado", user.getUsuario().getImagen());
+                model.addAttribute("misAgencias", user.getMapPermisosAgencia().keySet());
+                model.addAttribute("misArtistas", user.getMapPermisosArtista().keySet());
+            }
     }
 
-    @ModelAttribute("misAgencias")
-    public Set<Long> getNumeroMisAgencias() {
-        if (isUserAutheticated()) {
-
-            return ((CustomAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
-                    .getMapPermisosAgencia().keySet();
-
-        }
-        return new HashSet<>();
-    }
-
-    @ModelAttribute("misArtistas")
-    public Set<Long> getNumeroMisArtistas() {
-        if (isUserAutheticated()) {
-
-            return ((CustomAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
-                    .getMapPermisosArtista().keySet();
-
-        }
-        return new HashSet<>();
-    }
-
-//    @ModelAttribute("artistasOcupacion")
-//    public Set<Long> getArtistasConPermisoOcupacion() {
-//        if (isUserAutheticated()) {
-//
-//            return ((CustomAuthenticatedUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
-//                    .getMapPermisosArtista().entrySet().stream()
-//                    .filter(entry -> entry.getValue().contains(PermisoArtistaEnum.OCUPACIONES.name()))
-//                    .map(Map.Entry::getKey)
-//                    .collect(Collectors.toSet());
-//
-//        }
-//        return new HashSet<>();
-//    }
 }
-
