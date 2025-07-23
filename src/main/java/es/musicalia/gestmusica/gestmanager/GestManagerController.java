c229cce3a14adaf1424a0fdbd0e94680d01440fe
dcabodevila <dcabodevila@gmail.com>
2025-07-23 16:28:55 +0200

    Añadir endpoint para sincronización desde sistemas legados (PHP) y ajustar condiciones en botones de formulario de ocupaciones.

--- src/main/java/es/musicalia/gestmusica/gestmanager/GestManagerController.java
+++ src/main/java/es/musicalia/gestmusica/gestmanager/GestManagerController.java
@@ -8,11 +8,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/gestmanager")
@@ -41,6 +41,37 @@ public class GestManagerController {
 
         // Confirmas recepción
         return ResponseEntity.ok("Datos recibidos correctamente");
+    }
+
+    @PostMapping(value = "/sincronizacion.php", consumes = "application/x-www-form-urlencoded")
+    public ResponseEntity<String> redirigirPhpLegacy(
+            @RequestParam Map<String, String> params
+    ) {
+
+        log.info("Recibida sincronizacion: "+ params.toString());
+
+        try {
+            DatosGestmanagerDTO datos = new DatosGestmanagerDTO(
+                    params.get("id_artista"),
+                    params.get("accion"),
+                    params.get("fecha"),
+                    params.get("descripcion"),
+                    params.get("poblacion"),
+                    params.get("municipio"),
+                    params.get("provincia"),
+                    params.get("pais"),
+                    params.get("nombre_local"),
+                    params.get("estado"),
+                    params.get("indicadores")
+            );
+
+            gestmanagerService.procesarDatosGestmanager(datos);
+
+            return ResponseEntity.ok("OK");
+        } catch (Exception e) {
+            log.error("Error procesando datos de Gestmanager", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERROR");
+        }
     }
 
 }
