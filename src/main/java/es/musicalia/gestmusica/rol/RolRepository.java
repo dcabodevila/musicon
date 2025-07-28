f3d01584f0380e5f4026f0583d3d6dc611ef8cae
dcabodevila <dcabodevila@gmail.com>
2025-07-28 20:45:05 +0200

    fixes

--- src/main/java/es/musicalia/gestmusica/rol/RolRepository.java
+++ src/main/java/es/musicalia/gestmusica/rol/RolRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public interface RolRepository extends JpaRepository<Rol, Long> {
 
-    @Query("select new es.musicalia.gestmusica.rol.RolRecord(u.id, u.nombre, u.descripcion, u.codigo) from Rol u where u.tipoRol=?1 order by u.nombre")
+    @Query("select new es.musicalia.gestmusica.rol.RolRecord(u.id, u.nombre, u.descripcion, u.codigo) from Rol u where u.tipoRol=?1 order by u.id")
     List<RolRecord> findAllUsuarioRecords(Integer tipoRol);
 
     @Query("select new es.musicalia.gestmusica.rol.RolRecord(u.id, u.nombre, u.descripcion, u.codigo) from Rol u where u.codigo=?1")
