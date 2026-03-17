89de715ef567a9638ca76095dca20eff396d0387
dcabodevila <dcabodevila@gmail.com>
2026-03-18 00:22:42 +0100

    ajustes y ampliación funcionalidades eventos públicos

--- sql/1.1.0/1.1.0-ddl.sql
+++ sql/1.1.0/1.1.0-ddl.sql
@@ -61,3 +61,33 @@ SELECT a.id, c.id
 FROM gestmusica.artista a
 CROSS JOIN gestmusica.ccaa c
 ON CONFLICT DO NOTHING;
+
+-- Renombrar columna hora_actuacion a hora_actuacion_desde
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_schema = 'gestmusica'
+          AND table_name = 'ocupacion'
+          AND column_name = 'hora_actuacion'
+    ) THEN
+ALTER TABLE gestmusica.ocupacion RENAME COLUMN hora_actuacion TO hora_actuacion_desde;
+END IF;
+END $$;
+
+
+
+-- Añadir columna hora_actuacion_hasta
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM information_schema.columns
+        WHERE table_schema = 'gestmusica'
+          AND table_name = 'ocupacion'
+          AND column_name = 'hora_actuacion_hasta'
+    ) THEN
+ALTER TABLE gestmusica.ocupacion ADD COLUMN hora_actuacion_hasta TIME;
+END IF;
+END $$;
